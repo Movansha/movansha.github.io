4b788d71cf484635d148e3e08eec54ff7fe1933e
Batuhan D. <186194896+Movansha@users.noreply.github.com>
2025-04-25 00:32:53 +0300

    Add files via upload

---
+++ js/load-profiles.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const file_to_read = "profiles.json";
+
+    fetch(file_to_read).then(response => response.json())
+    .then(data => {
+
+        const container = document.querySelector(".about-me-container > #links");
+
+        data.forEach(item => {
+            const link = document.createElement("a");
+
+            const icon = document.createElement("img");
+            const title = document.createElement("p");
+
+            // ----------
+            
+            link.href = item.link;
+            link.target = "_blank";
+            link.draggable = "true";
+
+            icon.src = item.icon;
+            title.textContent = item.title;
+
+            link.appendChild(icon);
+            link.appendChild(title);
+
+            container.appendChild(link);
+        });
+
+    }).catch(error => console.error("Contents in " + file_to_read + " couldn't loaded!", error));
+});
