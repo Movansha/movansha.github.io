cccdca0e4691a6f24fc1b7866e6ecb9a55b9f1fe
Batuhan D. <186194896+Movansha@users.noreply.github.com>
2025-04-26 20:36:00 +0300

    Update change-theme.js

--- js/change-theme.js
+++ js/change-theme.js
@@ -5,13 +5,13 @@ function switch_theme() {
     if (change_theme == "dark") {
         document.documentElement.classList.add("dark");
         button_icon.src = "images/buttons/moon.webp"
-        button_icon.alt = "Ay simgesi";
+        button_icon.alt = "Moon icon";
     }
 
     else {
         document.documentElement.classList.remove("dark");
         button_icon.src = "images/buttons/sun.webp"
-        button_icon.alt = "Güneş simgesi";
+        button_icon.alt = "Sun icon";
     }
 
     localStorage.setItem("theme", change_theme);
